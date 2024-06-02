f8a0b135a2d4d1f0d91e6cd4146b74557a847172
frank <fyxhit2021@163.com>
2024-06-02 23:27:45 +0800

    Add a simple login page.

--- rems_frontend/src/main.js
+++ rems_frontend/src/main.js
@@ -1,10 +1,11 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
