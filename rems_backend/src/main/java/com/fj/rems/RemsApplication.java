ec940afcedfb8b734b1c9b7b5f4ac73e61050269
FangJian648 <162584284+FangJian648@users.noreply.github.com>
2024-06-02 23:22:20 +0800

    update name

--- rems_backend/src/main/java/com/fj/rems/RemsApplication.java
+++ rems_backend/src/main/java/com/fj/rems/RemsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectHeimaApplication {
+public class RemsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectHeimaApplication.class, args);
+        SpringApplication.run(RemsApplication.class, args);
     }
 
 }
