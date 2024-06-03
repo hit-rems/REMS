5245c989337ea86c27242aed4d2afa1742ba6817
FangJian648 <162584284+FangJian648@users.noreply.github.com>
2024-06-03 13:18:46 +0800

    update back

---
+++ rems_backend/src/main/java/com/fj/rems_backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.fj.rems_backend.config;
+
+import com.fj.rems.interceptors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register");
+    }
+}
---
+++ rems_backend/src/main/java/com/fj/rems_backend/validation/StateValidation.java
@@ -0,0 +1,15 @@
+package com.fj.rems_backend.validation;
+
+import com.fj.rems.anno.State;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class StateValidation implements ConstraintValidator<State,String> {
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (s==null){
+            return false;
+        }
+        return s.equals("已发布") || s.equals("草稿");
+    }
+}
