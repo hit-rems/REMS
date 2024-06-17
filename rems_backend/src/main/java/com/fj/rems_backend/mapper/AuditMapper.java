31f01121239aa7a0d0178f05f8e29b14660f3db4
FangJian648 <162584284+FangJian648@users.noreply.github.com>
2024-06-17 16:26:27 +0800

    Update AuditMapper.java

--- rems_backend/src/main/java/com/fj/rems_backend/mapper/AuditMapper.java
+++ rems_backend/src/main/java/com/fj/rems_backend/mapper/AuditMapper.java
@@ -1,6 +1,7 @@
 package com.fj.rems_backend.mapper;
 
 import com.fj.rems_backend.pojo.Audit;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -13,6 +14,7 @@ public interface AuditMapper {
     List<Audit> list(String type, int id,String role,String department);
 
     //查询不同状态的申请单数量
+    @MapKey("status")
     List<Map<String,Object>> countStatus(int id,String role,String department);
 
     @Update("update book set status=#{status} where id=#{id}")
@@ -23,7 +25,7 @@ public interface AuditMapper {
 
     @Select("<script>"
             + "select count(*) from book"
-            + " where status='已通过' AND end_time > now() AND (user_id=#{id} OR #{role} <> '学生')"
+            + " where status='已通过' AND end_time > now() AND (user_id=#{id} OR !#{role}.equals('学生')"
             + "<if test='department != null'>"
             + " AND department=#{department}"
             + "</if>"
