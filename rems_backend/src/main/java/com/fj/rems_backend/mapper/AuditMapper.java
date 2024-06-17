e47ea857b28d9e632c90d6e450533f79745a9d9d
FangJian648 <162584284+FangJian648@users.noreply.github.com>
2024-06-17 16:41:14 +0800

    1

--- rems_backend/src/main/java/com/fj/rems_backend/mapper/AuditMapper.java
+++ rems_backend/src/main/java/com/fj/rems_backend/mapper/AuditMapper.java
@@ -23,13 +23,6 @@ public interface AuditMapper {
     @Update("<script>update book set status=#{status} where id in <foreach collection='idList' item='id' open='(' separator=',' close=')'>#{id}</foreach></script>")
     void updatelist(List<Integer> idList, String status);
 
-    @Select("<script>"
-            + "select count(*) from book"
-            + " where status='已通过' AND end_time > now() AND (user_id=#{id} OR #{role} != '学生')"
-            + "<if test='department != null'>"
-            + " AND department=#{department}"
-            + "</if>"
-            + "</script>")
     int countWaitToUse(int id, String role, String department);
 
 
